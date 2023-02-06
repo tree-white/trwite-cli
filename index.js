5e5d9fe643ad1da20cfb42314a7bfe79c27d162b
徐泳楷 <treewhite@yeah.net>
2023-02-06 21:45:15 +0800

    fix 新增模版

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const TEMPLATES = {
     repository: 'github:tree-white/trwite-cli#rollup+ts',
     description: '初始Rollup打包工具 + TS超集 项目模版,主要用于开发组件/依赖库',
     replacePath: ['package.json', 'dome.html', 'rollup.config.mjs']
+  },
+  nestjs: {
+    repository: 'github:tree-white/trwite-cli#nestjs',
+    description: 'NestJs + TS + MySql 初始包',
+    replacePath: ['package.json']
   }
 }
 const COMMAND = {
