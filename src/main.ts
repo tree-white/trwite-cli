6f9247a052cdca5ba23364eb0411c234e5ebe6a2
徐泳楷 <treewhite@yeah.net>
2023-02-11 12:11:14 +0800

    fix 使用NestJs自带的Cors解决跨域问题

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { setupGlobalMiddleware } from './middleware/global';
 import { setupSwagger } from './plugins/swagger';
-import { setupCors } from './plugins/cors';
 import { setupGlobalHooks } from './global/hooks';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // 引入 cors 解决跨域
-  setupCors(app);
+  app.enableCors()
+
   // 引入全局中间件
   setupGlobalMiddleware(app);
   // 引入全局Hook
--- src/plugins/cors/index.ts
+++
@@ -1,6 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-import * as cors from 'cors';
-
-export function setupCors(app: INestApplication) {
-  app.use(cors());
-}
